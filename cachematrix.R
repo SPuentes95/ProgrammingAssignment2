91c6a0e24b92417862d1c30da7e5158f254920cc
SPuentes95 <santiago_p_n@hotmail.com>
2019-08-18 18:28:31 -0500

    Simplified version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of this code is to create functions that can save a matrix in cache for future use
+## as well as a function to calculate the inverse of a matrix, or to retrieve it from memory if already calculated
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function that returns a list of functions to create a matrix, retrieve it, or create and retrieve its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <<- NULL
+  mat_obj <- function() x
+  setinvr <- function(inverse) inv <<- inverse
+  getinvr <- function() inv
+  list(mat_obj = mat_obj,
+       setinvr = setinvr,
+       getinvr = getinvr)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function that either calculates the inverse of a given matrix, or to retrieve the inverse if it already has been calculated
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inver <- x$getinvr()
+  if(!is.null(inver)){
+    print("LOL")
+    return(inver)
+  }
+  matr <- x$mat_obj()
+  solution <- solve(matr)
+  x$setinvr(solution)
+  solution
 }
